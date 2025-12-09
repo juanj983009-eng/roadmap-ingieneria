e26f81e29e0c593497372ba5a7d2f80746862038
Juan Parra Terrel <juanj983009@gmail.com>
2025-12-09 11:09:08 -0500

    Primer commit: Configuración de entorno lista

---
+++ setup_test.py
@@ -0,0 +1,11 @@
+import sys
+import datetime
+
+nombre = "Futuro Staff Engineer"
+fecha = datetime.datetime.now()
+
+print("--- REPORTE DE SISTEMA ---")
+print(f"Usuario: {nombre}")
+print(f"Fecha de inicio: {fecha}")
+print(f"Versión de Python detectada: {sys.version}")
+print("--- SISTEMA OPERATIVO Y LISTO ---")
