f525ef3acd7c7b267949e6593775ff6a667c900b
Juan Parra Terrel <juanj983009@gmail.com>
2025-12-12 19:15:25 -0500

    Ejercicio 3 de la clase 02

---
+++ 02_3_ejercicio.py
@@ -0,0 +1,7 @@
+# Ejercicio 2.3: Conversor de temperaturas
+
+temperatura_celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+temperatura_fahrenheit = temperatura_celsius *1.8 +32
+print(f"La temperatura en grados Fahrenheit es: {temperatura_fahrenheit}")
+temperatura_kelvin = temperatura_celsius + 273.15
+print(f"La temperatura en grados Kelvin es: {temperatura_kelvin}")
