2d0ced797afc29b8082a399b3808cd9a243f7330
Juan Parra Terrel <juanj983009@gmail.com>
2025-12-12 20:41:07 -0500

    Ejercicio 1 de la clase 03

--- 0.3_1_ejercicio.py
+++ 0.3_1_ejercicio.py
@@ -1,11 +1,12 @@
-agrega_tu_nota = input("¿Cuanto fue tu nota del curso?:")
-agrega_tu_nota_float = float(agrega_tu_nota)
+# Ejercicio 3.1: Evaluación de notas
 
-if agrega_tu_nota_float <= 20.0 and agrega_tu_nota_float >= 17.0:
+agrega_tu_nota = float(input("¿Cuanto fue tu nota del curso?:"))
+
+if agrega_tu_nota <= 20.0 and agrega_tu_nota >= 17.0:
     print("¡Felicitaciones! Eres excelente.")
-elif agrega_tu_nota_float < 17.0 and agrega_tu_nota_float >= 11.0:
+elif agrega_tu_nota  < 17.0 and agrega_tu_nota >= 11.0:
     print("¡Bien hecho! Has aprobado.")
-elif agrega_tu_nota_float < 0 or agrega_tu_nota_float > 20:
+elif agrega_tu_nota < 0 or agrega_tu_nota > 20:
     print("Error: La nota ingresada no es válida. Debe estar entre 0 y 20.")
 else:
     print("Lo siento, no has aprobado el curso. ¡Sigue intentando!")
