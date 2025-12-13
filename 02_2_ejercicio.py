2d03c0c531b43bacca0358e0109e8e188a8c9b2c
Juan Parra Terrel <juanj983009@gmail.com>
2025-12-12 19:07:38 -0500

    Ejercicio 2 de la clase 02

---
+++ 02_2_ejercicio.py
@@ -0,0 +1,8 @@
+nombre_producto = input("Ingrese el nombre del producto: ")
+precio_unitario = float(input("Ingrese el precio unitario del producto: "))
+cantidad = int(input("Ingrese la cantidad a comprar: "))
+
+subtotal = precio_unitario * cantidad
+total = subtotal * 1.18  # Aplicando IGV del 18%
+print(f"Subtotal: S/ {subtotal:.2f}")
+print(f"Total a pagar (con IGV): S/ {total:.2f}")
