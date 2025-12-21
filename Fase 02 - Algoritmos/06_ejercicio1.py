41ca9a51ef1932a6acb75b7561777fba2a76a4c5
Juan Parra Terrel <juanj983009@gmail.com>
2025-12-20 22:35:27 -0500

    Buscador de precios

--- Fase 02 - Algoritmos/06_ejercicio1.py
+++ Fase 02 - Algoritmos/06_ejercicio1.py
@@ -6,13 +6,7 @@ inventario = [
     {"nombre": "pera", "precio": 0.6},
     {"nombre": "uva", "precio": 1.0}
 ]
-producto = input("Ingresa el nombre del producto: ").strip().lower()
-encontrado = False
-for item in inventario:
-    if item["nombre"] == producto:
-        print(f"El precio de la {producto} es: ${item['precio']}")
-        encontrado = True
-        break
-if not encontrado:
-    print("Producto no encontrado en el inventario.")
 
+for item in inventario:
+    if item["precio"] > 0.5:
+        print(f"El precio de {item['nombre']} es {item['precio']}")
